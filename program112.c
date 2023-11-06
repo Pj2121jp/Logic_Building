7f890322686b7cd93216d6b785ec3d3a1a18d180
Pj2121jp <prasadpatil24212@gmail.com>
2023-11-06 19:21:12 +0530

    today

---
+++ program112.c
@@ -0,0 +1,15 @@
+ #include<stdio.h>
+ 
+
+ int main()
+ {
+    char ch = '\0';
+
+    printf("Enter the charecer : ");
+    scanf("%c",&ch);
+
+     printf("Entered charecer is %d : ",ch);
+
+
+    return 0;
+ }
