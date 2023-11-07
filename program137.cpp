a4744278f9fbce81a8684897251c57b6c5ec2b79
Pj2121jp <prasadpatil24212@gmail.com>
2023-11-07 19:31:21 +0530

    cmt

--- program137.cpp
+++ program137.cpp
@@ -1,10 +1,31 @@
 #include<iostream>
 using namespace std;
 
+int Factorial(int iNo)
+{
+    int iFact = 1;
+
+    for(int iCnt = 1; iCnt <= iNo; iCnt ++)
+    {
+        iFact=iFact*iCnt;
+    }
+
+    return iFact;
+
+}
+
 int main()
 {
-    cout<<"Jay Ganesh... \n";
-    cout<<"Jay Ganesh..."<<endl;
+    int iValue = 0;
+    int iRet = 0;
+
+    cout<<"Enter the number"<<endl;
+    cin>>iValue;
+
+    iRet = Factorial(iValue);
+
+    cout<<"Factorial is : "<<iRet<<endl;
+
 
     return 0;
 }
